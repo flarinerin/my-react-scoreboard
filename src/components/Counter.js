a6a561e7eb2255111f63a556b6c04cb9046f6078
flarinerin <erin.schmidt@gmail.com>
2019-02-22 15:50:22 -0800

    work for lifting data

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,31 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Counter extends React.Component {
-  state = {
-    score: 0
-  };
+const Counter = (props) => {
+  //
+  // incrementScore = () => {
+  //   this.setState( prevState => ({
+  //     score: prevState.score + 1
+  //   }));
+  // }
+  //
+  // decrementScore = () => {
+  //   this.setState( prevState => ({
+  //     score: prevState.score - 1
+  //   }));
+  // }
 
-  incrementScore = () => {
-    this.setState( prevState => ({
-      score: prevState.score + 1
-    }));
-  }
-
-  decrementScore = () => {
-    this.setState( prevState => ({
-      score: prevState.score - 1
-    }));
-  }
-
-  render() {
-    return (
-      <div className="counter">
-        <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
-        <span className="counter-score">{ this.state.score }</span>
-        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
-      </div>
-    );
-  }
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <span className="counter-score">{ props.score }</span>
+      <button className="counter-action increment"> + </button>
+    </div>
+  );
 }
 
 export default Counter;
